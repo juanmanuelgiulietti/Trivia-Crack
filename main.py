f8bebe438a2bab084c043cdc434f9ce0c8a18061
Juan Manuel Giulietti <juanmanuelgiulietti2@gmail.com>
2025-05-04 22:07:24 -0300

    Funcion determinarQuienComienza(usuario)

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import random
+
+def determinarQuienComienza(usuario):
+    """
+    La funcion jugadorQueComienza(usuario) elige aleatoreamente desde una lista de jugadores, quien empieza a jugar.
+    """
+    jugadores = [usuario, "Computadora"]
+    jugadorQueComienza = random.choice(jugadores)
+    return jugadorQueComienza
+
 def iniciarJuego():
     """
     La funcion iniciarJuego() le da la bienvenida al usuario, pide el ingreso de un nombre y valida que la entrada sea valida.
     """
-    usuario = str(input("Ingrese su nombre para iniciar la partida: "))
+    usuario = str(input("Ingrese su nombre para iniciar la partida: ")).capitalize()
     while usuario == " ":
         print("Por favor ingrese un nombre valido.")
-        usuario = str(input("Ingrese su nombre para iniciar la partida: "))
+        usuario = str(input("Ingrese su nombre para iniciar la partida: ")).capitalize()
     return usuario
 
 def main():
@@ -16,4 +26,7 @@ def main():
     print(f"Bienvenido al juego de Preguntados!\n")
     usuario = iniciarJuego()
     print(f"\nHola {usuario}!")
+    
+    jugadorQueComienza = determinarQuienComienza(usuario)
+    print(f"\nTurno de comenzar: {jugadorQueComienza}")
 main()
