c8fc19cb47567058a71b6791e2efa53247df4151
Juan Manuel Giulietti <juanmanuelgiulietti2@gmail.com>
2025-05-08 19:08:38 -0300

    Agrego las funciones gestionarTurnos(jugadorQueComienza, usuario) y girarRuleta() y documento estas

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import random
+import requests
+
+def girarRuleta():
+    """
+    La funcion girarRuleta() elige aleatoreamente mediante un random.choice de la lista "categorias" una categoria para jugar.
+    """
+    categorias = ["Arte", "Ciencia", "Deportes", "Entretenimiento", "Geografia", "Historia", "Corona"]
+    resultadoDeLaRuleta = random.choice(categorias)
+    return resultadoDeLaRuleta
+
+def gestionarTurnos(jugadorQueComienza, usuario):
+    """
+    La funcion gestionarTurnos(jugadorQueComienza, usuario) gestiona los turnos de los jugadores para ver quien comienza la partida.
+    """
+    if jugadorQueComienza == usuario:
+        print(f"Comienza jugando: {jugadorQueComienza}")
+    else:
+        print(f"Comienza jugando: Computadora")
 
 def determinarQuienComienza(usuario):
     """
@@ -29,4 +47,7 @@ def main():
     
     jugadorQueComienza = determinarQuienComienza(usuario)
     print(f"\nTurno de comenzar: {jugadorQueComienza}")
+    
+    resultadoDeLaRuleta = girarRuleta()
+    print(f"🎡 Resultado de la ruleta: {resultadoDeLaRuleta}\n")
 main()
